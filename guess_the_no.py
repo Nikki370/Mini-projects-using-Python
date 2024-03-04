69f4704e965eabad7ffe1d0c9ddf51609e1c92bd
Mini projects using Python <np810652@gmail.com>
2024-03-04 11:17:54 +0530

    add new file

---
+++ guess_the_no.py
@@ -0,0 +1,20 @@
+import numpy as np
+target = np.random.randint(1, 100)
+
+while (target):
+    user = input("User's choice: or Ouit{Q}: ")
+    if user == "Q":
+        break
+    
+    user = int(user)
+    if user == target:
+        print("HURRAH :) you won!")
+        break
+    elif user > target:
+        print("Smaller number please...")
+    else:
+        print("Larger number Please...")
+
+print("computer:", target)
+
+print("GAME OVER!!!")
